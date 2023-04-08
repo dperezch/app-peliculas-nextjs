d55864b1af9ff651b25c73b4f74404eb10067c27
dperezch <davarmando@gmail.com>
2023-04-08 12:46:08 -0400

    config URL & get data

---
+++ pages/api/peliculas/index.js
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const {peliculas} = require('./datos.json')
+
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json(peliculas)
+  }else {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).json({message: `Método ${req.method} no permitido`})
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
-
-
 import Layout from '@/components/Layout'
+import { URL_API } from '@/config';
 
 
-export default function Home() {
+export default function Home({peliculas}) {
+
+  console.log(peliculas);
   return (
     <Layout>
-      <h1>Aplicación de Películas</h1>
-      <p>Aquí lista de 3 películas</p>
+      <h1 className='text-center mt-4' >Aplicación de Películas</h1>
+      {peliculas.map((pelicula)=>(
+        <h3 key={pelicula.id}> {pelicula.titulo} </h3>
+      ))}
+      
     </Layout>
   )
 }
+
+//ejemplo 1: getServerSideProps
+export async function getServerSideProps(){
+  const res = await fetch(`${URL_API}/api/peliculas`)
+  const peliculas = await res.json()
+
+  return{
+    props: {peliculas}
+  }
+}
+
+//ejemplo 2: getStaticProps
+/* export async function getStaticProps(){
+  const res = await fetch(`${URL_API}/api/peliculas`)
+  const peliculas = await res.json()
+
+  return{
+    props: {peliculas},
+    revalidate: 2
+  }
+} */
